b62b4907e4f74ddaefbd60ea14e3030c9c7b3bed
Yusuf KEF <41286150+kefyusuf@users.noreply.github.com>
2026-09-18 12:10:05 +0300

    style: satisfy Pint for M2 core contracts

--- src/Contracts/Exception/BloomFilterNotProvisioned.php
+++ src/Contracts/Exception/BloomFilterNotProvisioned.php
@@ -6,6 +6,4 @@ namespace Kefyusuf\BloomGate\Contracts\Exception;
 
 use RuntimeException;
 
-final class BloomFilterNotProvisioned extends RuntimeException
-{
-}
+final class BloomFilterNotProvisioned extends RuntimeException {}
