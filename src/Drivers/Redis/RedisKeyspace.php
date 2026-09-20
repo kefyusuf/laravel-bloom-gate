e37ac523ce8ded346c17dc0f552b14994ed76198
Yusuf KEF <41286150+kefyusuf@users.noreply.github.com>
2026-09-20 10:06:36 +0300

    feat(redis): add strict control state format

--- src/Drivers/Redis/RedisKeyspace.php
+++ src/Drivers/Redis/RedisKeyspace.php
@@ -23,6 +23,15 @@ final readonly class RedisKeyspace
         return new self($prefix);
     }
 
+    public function stateKey(FilterName $name): string
+    {
+        return sprintf(
+            '%s:{%s}:state',
+            $this->prefix,
+            $name->value(),
+        );
+    }
+
     public function metaKey(
         FilterName $name,
         FilterVersion $version,
