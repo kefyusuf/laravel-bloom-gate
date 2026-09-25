d699d6b101a5ec5acd2ef71c2c0f75518bd2fa85
Yusuf KEF <41286150+kefyusuf@users.noreply.github.com>
2026-09-25 11:04:40 +0300

    style(laravel): preflight facade phpdoc

--- src/Laravel/Facades/BloomGate.php
+++ src/Laravel/Facades/BloomGate.php
@@ -12,7 +12,7 @@ use Kefyusuf\BloomGate\Laravel\BloomGateManager;
  * @method static bool exists(string $filter, string|int $value)
  * @method static ExistenceResult existsResult(string $filter, string|int $value)
  * @method static void add(string $filter, string|int $value)
- * @method static void addMany(string $filter, iterable $values)
+ * @method static void addMany(string $filter, iterable<string|int> $values)
  *
  * @see BloomGateManager
  */
